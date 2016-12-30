94293aa1ec475a88402a8b3a6f7bd72f15016550
Kevin <kevin.walchko@outlook.com>
2016-12-29 22:46:11 -0700

    Try again

--- opencvutils/__init__.py
+++ opencvutils/__init__.py
@@ -6,7 +6,7 @@ from .convenience import rotate
 from .convenience import resize
 from .convenience import skeletonize
 from .convenience import opencv2matplotlib
-from .convenience import url_to_image
+# from .convenience import url_to_image
 from .convenience import auto_canny
 from .convenience import is_cv2
 from .convenience import is_cv3
