258348b165c3425488bbda3bd2450671a4e11b4b
Kevin <kevin.walchko@outlook.com>
2017-01-01 15:22:49 -0700

    t

--- install-opencv.sh
+++ install-opencv.sh
@@ -4,10 +4,10 @@ set -ex
 
 ls -alh
 
-wget https://github.com/opencv/opencv/archive/3.2.0.tar.gz
+#wget https://github.com/opencv/opencv/archive/3.2.0.tar.gz
 
-ls -alh
-mv 3.2.0.tar.gz opencv-3.2.0.tar.gz
-tar -xzf opencv-3.2.0.tar.gz
-mkdir opencv-3.2.0/build
-cd opencv-3.2.0/build && cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local .. && make -j7 && make install
+#ls -alh
+#mv 3.2.0.tar.gz opencv-3.2.0.tar.gz
+#tar -xzf opencv-3.2.0.tar.gz
+#mkdir opencv-3.2.0/build
+cd opencv-3.2.0/build && cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../.. .. && make -j7 && make install
