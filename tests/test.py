fe9e9ad352aa8d6735b87d28aec10043620bd5b5
Kevin <kevin.walchko@outlook.com>
2017-01-01 00:58:50 -0700

    test

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 import opencvutils as cvu
+from opencvutils.video import Camera
 
 def test_dummy():
+	c = Camera('pi')
+	c.init()
+	ret, im = c.read()
+	if ret:
+		im = cvu.translate(im, 10, 10)
 	assert True
