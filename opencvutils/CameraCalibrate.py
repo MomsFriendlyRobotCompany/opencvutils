9fe74d1156f91e0e2ba3a36c11ea2ae02f65c0c0
walchko <walchko@users.noreply.github.com>
2018-01-23 14:50:28 -0700

    simplify

--- opencvutils/CameraCalibrate.py
+++ opencvutils/CameraCalibrate.py
@@ -11,12 +11,14 @@ class CameraCalibration(object):
 	'''
 	marker_checkerboard = True
 	marker_size = None
+	save_cal_imgs = []
 
 	def __init__(self, show_markers=True):
 		self.show_markers = show_markers
+		self.save_cal_imgs = []
 
 	def __del__(self):
-		cv2.destroyAllWindows()
+		pass
 
 	# # write camera calibration file out
 	def save(self, save_file):
@@ -49,6 +51,7 @@ class CameraCalibration(object):
 		print('RMS error:', self.data['rms'])
 
 	# Pass a gray scale image and find the markers (i.e., checkerboard, circles)
+	# TODO: make objpoints and imgpoints a class member
 	def findMarkers(self, gray, objpoints, imgpoints):
 		# objp = np.zeros((self.marker_size[0]*self.marker_size[1],3), np.float32)
 		# objp[:,:2] = np.mgrid[0:self.marker_size[0],0:self.marker_size[1]].T.reshape(-1,2)
@@ -61,20 +64,13 @@ class CameraCalibration(object):
 		else:
 			ret, corners = cv2.findCirclesGrid(gray, self.marker_size, flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
 
-		# If found, add object points, image points (after refining them)
 		if ret:
-			print('[+] found ', corners.size / 2, 'of', self.marker_size[0]*self.marker_size[1], 'corners')
-			term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
-			cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
 			imgpoints.append(corners.reshape(-1, 2))
 			objpoints.append(objp)
-
-			# Draw the corners
-			self.draw(gray, corners)
 		else:
-			print('[-] Could not find markers')
+			corners = [] # didn't find any
 
-		return ret, objpoints, imgpoints
+		return ret, objpoints, imgpoints, corners
 
 	# draw the detected corners on the image for display
 	def draw(self, image, corners):
@@ -83,48 +79,75 @@ class CameraCalibration(object):
 		# Draw and display the corners
 		if corners is not None:
 			cv2.drawChessboardCorners(image, self.marker_size, corners, True)
-		cv2.imshow('camera', image)
-		cv2.waitKey(500)
+
 		return image
 
 	# use a calibration matrix to undistort an image
-	def undistort(self, image):
-		# undistort
-		dst = cv2.undistort(image, self.data['camera_matrix'], self.data['dist_coeff'], None, self.data['newcameramtx'])
-		return dst
-
-	# run the calibration process on a series of images
-	def calibrate(self, images, alpha=0.5, marker_size=(9, 6)):
+	def undistort(self, image, alpha):
 		"""
-		images: an array of calibration images, all assumed to be the same size
+		image: an image
 
 		alpha = 0: returns undistored image with minimum unwanted pixels (image
 					pixels at corners/edges could be missing)
 		alpha = 1: retains all image pixels but there will be black to make up
 					for warped image correction
 		"""
-
-		self.marker_size = marker_size
-
-		# Arrays to store object points and image points from all the images.
-		objpoints = []  # 3d point in real world space
-		imgpoints = []  # 2d points in image plane.
-		w, h = 0, 0
-
-		for fname in images:
-			gray = cv2.imread(fname, 0)
-			_, objpoints, imgpoints = self.findMarkers(gray, objpoints, imgpoints)
-			h, w = gray.shape[:2]
-			# print(fname,h,w)
-
-		# print len(objpoints),len(imgpoints),w,h
-
-		rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, (w, h), None, None)
-
+		h,w = image.shape[:2]
+		mtx = self.data['camera_matrix']
+		dist = self.data['dist_coeff']
 		# Adjust the calibrations matrix
 		# alpha=0: returns undistored image with minimum unwanted pixels (image pixels at corners/edges could be missing)
 		# alpha=1: retains all image pixels but there will be black to make up for warped image correction
 		# returns new cal matrix and an ROI to crop out the black edges
 		newcameramtx, _ = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), alpha)
-		self.data = {'camera_matrix': mtx, 'dist_coeff': dist, 'newcameramtx': newcameramtx, 'rms': rms, 'rvecs': rvecs, 'tvecs': tvecs}
+		# undistort
+		ret = cv2.undistort(image, mtx, dist, None, newcameramtx)
+		return ret
+
+	# run the calibration process on a series of images
+	def calibrate(self, images, marker_size=(9, 6)):
+		"""
+		images: an array of grayscale images, all assumed to be the same size
+		"""
+
+		self.marker_size = marker_size
+		self.save_cal_imgs = []
+
+		# Arrays to store object points and image points from all the images.
+		objpoints = []  # 3d point in real world space
+		imgpoints = []  # 2d points in image plane.
+
+		max_corners = self.marker_size[0]*self.marker_size[1]
+
+		for cnt, gray in enumerate(images):
+			orig = gray.copy()
+			if len(gray.shape) > 2:
+				gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+
+			ret, objpoints, imgpoints, corners = self.findMarkers(gray, objpoints, imgpoints)
+			# If found, add object points, image points (after refining them)
+			if ret:
+				print('[{}] + found {} of {} corners'.format(cnt, corners.size / 2, max_corners))
+				term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+				cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
+
+				# Draw the corners
+				orig = self.draw(orig, corners)
+				self.save_cal_imgs.append(orig)
+			else:
+				print('[{}] - Could not find markers'.format(cnt))
+
+		# h, w = images[0].shape[:2]
+		# rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, (w, h), None, None)
+
+		# images size here is backwards: w,h
+		rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+		# Adjust the calibrations matrix
+		# alpha=0: returns undistored image with minimum unwanted pixels (image pixels at corners/edges could be missing)
+		# alpha=1: retains all image pixels but there will be black to make up for warped image correction
+		# returns new cal matrix and an ROI to crop out the black edges
+		# newcameramtx, _ = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), alpha)
+		# self.data = {'camera_matrix': mtx, 'dist_coeff': dist, 'newcameramtx': newcameramtx, 'rms': rms, 'rvecs': rvecs, 'tvecs': tvecs}
+		self.data = {'camera_matrix': mtx, 'dist_coeff': dist, 'rms': rms}
 		return self.data
